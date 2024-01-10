4eaee0742ffa15c72eef9db462289bd63a6fbed6
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-01-10 15:36:53 -0700

    Added changes to Prep 3 including assignement instructions to build number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+
+        string keepPlaying = "yes";
+        int guessCount = 0;
+        int userGuess = 0;
+
+        while (keepPlaying == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,101);
+
+            while (userGuess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string ug = Console.ReadLine();
+                userGuess = int.Parse(ug);
+                guessCount = guessCount + 1;
+
+                if (magicNumber < userGuess) 
+                {
+                    Console.Write("Lower");
+                    Console.WriteLine("");
+                }
+                else if (magicNumber == userGuess)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine("");
+
+                }
+                else
+                {
+                    Console.Write("Higher");
+                    Console.WriteLine("");
+                }
+            }
+            Console.Write("Would you like to keep playing? ");
+            keepPlaying = Console.ReadLine();
+        }
+        Console.WriteLine($"It took you {guessCount} guesses.");
+        Console.WriteLine("Thank you for playing!");
     }
 }
