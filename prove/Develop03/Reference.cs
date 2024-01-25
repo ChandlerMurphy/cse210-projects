e495e24c208cb3c2b0ea7adbe7caf59a91ac916b
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-01-24 19:09:58 -0700

    Stubbed out Encapsulation Design Activity

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,23 @@
+using System.Runtime.Serialization;
+
+public class Reference {
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    Reference(string book, int chapter, int verse)
+    {
+
+    }
+
+    Reference(string book, int chapter, int startVerse, int endVerse) 
+    {
+
+    }
+
+    public string GetDisplayText() 
+    {
+        return "";
+    }
+}
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,24 @@
+public class Scripture {
+    private Reference _reference; 
+    private List<Word> _words;
+
+    Scripture(Reference Reference, string text) 
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide) 
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCommpletelyHidden()
+    {
+        return true;
+    }
+}
