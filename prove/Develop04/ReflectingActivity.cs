b9865a154dfd8982c8b04779068690219b7a82f3
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-01-31 17:57:33 -0700

    Stubbed out Inheritance Design Activity

---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,35 @@
+public class ReflectingActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public ReflectingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "string";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "string";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestions()
+    {
+        
+    }
+}
