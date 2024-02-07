bd71cffe96304809aa6fefb042ccfdda2c2b66ca
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-02-07 09:09:09 -0700

    Completed W5 Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.WriteLine("Hello Learning05 World!");
+
+        Square square = new Square("green", 4);
+
+        Rectangle rectangle = new Rectangle("blue", 6, 2);
+
+        Circle circle = new Circle("red", 8);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
