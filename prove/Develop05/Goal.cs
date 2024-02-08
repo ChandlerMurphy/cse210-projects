74a669289976dc688c464f5226eada350674105b
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-02-07 18:18:35 -0700

    Stubbed out W05 Develop: Eternal Quest Program

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,22 @@
+public abstract class Goal 
+{
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
+
+    public Goal (string name, string description, int points)
+    {
+
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public virtual string GetDetailsString()
+    {
+        return "";
+    }
+
+    public abstract string GetStringRepresentation();
+}
