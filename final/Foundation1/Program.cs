6253fcfce780cbf320fcd39056998b96a54ece77
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-02-13 08:51:46 -0700

    Stubbed out Foundation 1 Project classes

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation1 World!");
+
+        List<Video> videos = new List<Video>();
+
+        
+
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,17 @@
+public class Video 
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> comment;
+
+    public Video (string title, string author, string length) 
+    {
+
+    }
+
+    public void AddComment() 
+    {
+
+    }
+}
